abccfbaf69a23392206c2ae03c040d3f6e541fa5
Kingsomsri45 <640112418053@bru.ac.th>
2024-03-05 20:13:39 +0700

    Update edit_room.dart

--- lib/edit_room.dart
+++ lib/edit_room.dart
@@ -1,13 +1,9 @@
-// ignore: unused_import
-import 'dart:ffi';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert'; // For using json.encode
 
 class RoomEditPage extends StatefulWidget {
-  // ignore: non_constant_identifier_names
   final int hotelId, roomId, number_of_rooms;
   final String type, amenities;
   final double price;
@@ -15,7 +11,6 @@ class RoomEditPage extends StatefulWidget {
   const RoomEditPage({
     Key? key,
     required this.hotelId,
-    // ignore: non_constant_identifier_names
     required this.number_of_rooms,
     required this.roomId,
     required this.type,
@@ -31,7 +26,6 @@ class _RoomEditPageState extends State<RoomEditPage> {
   final _formKey = GlobalKey<FormState>();
   late TextEditingController _hotelIdController,
       _roomIdController,
-      // ignore: non_constant_identifier_names
       _number_of_roomsController,
       _typeController,
       _amenitiesController,
@@ -43,14 +37,12 @@ class _RoomEditPageState extends State<RoomEditPage> {
     _hotelIdController = TextEditingController(text: widget.hotelId.toString());
     _roomIdController = TextEditingController(text: widget.roomId.toString());
     _typeController = TextEditingController(text: widget.type);
-    _number_of_roomsController =
-        TextEditingController(text: widget.number_of_rooms.toString());
+    _number_of_roomsController = TextEditingController(text: widget.number_of_rooms.toString());
     _amenitiesController = TextEditingController(text: widget.amenities);
     _priceController = TextEditingController(text: widget.price.toString());
   }
 
-  Future<void> _editRoom(int hotelId, roomId, numberOfRooms, String type,
-      String amenities, double price) async {
+  Future<void> _editRoom(int hotelId, roomId, numberOfRooms, String type, String amenities, double price) async {
     if (kDebugMode) {
       print('Updating room with ID: $roomId');
     }
@@ -59,9 +51,7 @@ class _RoomEditPageState extends State<RoomEditPage> {
     try {
       var response = await http.put(
         url,
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        headers: {'Content-Type': 'application/json'},
         body: json.encode({
           'hotelId': hotelId,
           'roomId': roomId,
@@ -73,13 +63,12 @@ class _RoomEditPageState extends State<RoomEditPage> {
       );
       if (response.statusCode == 200) {
         if (kDebugMode) {
-          print('Hotel updated successfully.');
+          print('Room updated successfully.');
         }
-        // Optionally, refresh the UI or user list here if needed
+        Navigator.of(context).pop(); // Optionally pop the context
       } else {
         if (kDebugMode) {
-          print(
-              'Failed to update Room data with status code: ${response.statusCode}');
+          print('Failed to update Room data with status code: ${response.statusCode}');
         }
       }
     } catch (e) {
@@ -99,110 +88,108 @@ class _RoomEditPageState extends State<RoomEditPage> {
           onPressed: () => Navigator.of(context).pop(),
         ),
       ),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: const EdgeInsets.all(16),
-          children: [
-            TextFormField(
-              controller: _hotelIdController,
-              decoration: const InputDecoration(labelText: 'Hotel id'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter an Hotel id';
-                }
-                return null;
-              },
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+                'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/22/25/ce/ea/kingsford-hotel-manila.jpg?w=1200&h=-1&s=1'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.85),
+              borderRadius: BorderRadius.circular(12),
             ),
-            TextFormField(
-              controller: _roomIdController,
-              decoration: const InputDecoration(labelText: 'Room id'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a Room id';
-                }
-                return null;
-              },
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: ListView(
+                  children: [
+                    TextFormField(
+                      controller: _hotelIdController,
+                      decoration: const InputDecoration(labelText: 'Hotel ID'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter an Hotel ID';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: _roomIdController,
+                      decoration: const InputDecoration(labelText: 'Room ID'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a Room ID';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: _typeController,
+                      decoration: const InputDecoration(labelText: 'Type'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a type';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: _number_of_roomsController,
+                      decoration: const InputDecoration(labelText: 'Number of Rooms'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter the number of rooms';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: _amenitiesController,
+                      decoration: const InputDecoration(labelText: 'Amenities'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter amenities';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: _priceController,
+                      decoration: const InputDecoration(labelText: 'Price'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a price';
+                        }
+                        return null;
+                      },
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          // Only proceed if the form is valid
+                          _editRoom(
+                            int.parse(_hotelIdController.text),
+                            int.parse(_roomIdController.text),
+                            int.parse(_number_of_roomsController.text),
+                            _typeController.text,
+                            _amenitiesController.text,
+                            double.parse(_priceController.text),
+                          );
+                        }
+                      },
+                      child: const Text('Save'),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            TextFormField(
-              controller: _typeController,
-              decoration: const InputDecoration(labelText: 'Type'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a Type';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _number_of_roomsController,
-              decoration: const InputDecoration(labelText: 'number of room'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a number of room';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _amenitiesController,
-              decoration: const InputDecoration(labelText: 'Amenities'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a Amenities';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _priceController,
-              decoration: const InputDecoration(labelText: 'Price'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a Price';
-                }
-                return null;
-              },
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Validate form first
-                if (_formKey.currentState!.validate()) {
-                  try {
-                    int hotelId = int.parse(_hotelIdController.text);
-                    int roomId = int.parse(_roomIdController.text);
-                    int numberOfRooms =
-                        int.parse(_number_of_roomsController.text);
-                    double price = double.parse(_priceController.text);
-
-                    _editRoom(
-                      hotelId,
-                      roomId,
-                      numberOfRooms,
-                      _typeController.text,
-                      _amenitiesController.text,
-                      price,
-                    ).then((_) {
-                      // Handle success or error here, if necessary
-                      Navigator.of(context)
-                          .pop(); // Assuming you want to pop on success
-                    }).catchError((error) {
-                      // Handle error
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text('Error updating room: $error')),
-                      );
-                    });
-                  } catch (e) {
-                    // Handle parsing error, e.g., show a Snackbar
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Invalid input: $e')),
-                    );
-                  }
-                }
-              },
-              child: const Text('Save'),
-            ),
-          ],
+          ),
         ),
       ),
     );
